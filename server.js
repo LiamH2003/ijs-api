1a8a60d21d36c02878650a97a811330cffb170c8
LiamH2003 <108660359+LiamH2003@users.noreply.github.com>
2026-07-29 15:19:35 +0200

    feat: initialize express server with mongodb connection

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB verbonden'))
+  .catch(err => console.error(err));
+
+app.get('/', (req, res) => res.send('API draait'));
+
+app.listen(process.env.PORT || 3000, () => console.log('Server gestart'));
